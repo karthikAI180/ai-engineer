8a343a728c721f19dade943debfc09da468406f4
karthikAI <karthikari180@gmail.com>
2026-05-24 17:23:54 -0400

    recursive function

--- notebooks/first.py
+++ notebooks/first.py
@@ -1,9 +1,17 @@
-# Q. Write a program to display unique vowels present in the given word?
-vowels=['a','e','i','o','u']
-word=input("Enter the word to search for vowels: ")
-k=[]
-for i in word:
-    if i in vowels:
-        if i not in k:
-            k.append(i)
-print(k)
+#1Q. Write a program to display unique vowels present in the given word?
+# vowels=['a','e','i','o','u']
+# word=input("Enter the word to search for vowels: ")
+# k=[]
+# for i in word:
+#     if i in vowels:
+#         if i not in k:
+#             k.append(i)
+# print(k)
+
+#2Q. Write a Python Function to find factorial of given number with recursion.
+def fact(n):
+    if n<1:
+        return 1
+    else:
+        return n*fact(n-1)
+print(fact(5))
