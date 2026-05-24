8fcbaca742f2e827fdf5f8057bf92fcaf128548a
karthikAI <karthikari180@gmail.com>
2026-05-24 16:34:08 -0400

    updated434pm

--- notebooks/first.py
+++ notebooks/first.py
@@ -1,3 +1,9 @@
-print("karthik ai journey starts")
-print("modified")
-print("branch")
+# Q. Write a program to display unique vowels present in the given word?
+vowels=['a','e','i','o','u']
+word=input("Enter the word to search for vowels: ")
+k=[]
+for i in word:
+    if i in vowels:
+        if i not in k:
+            k.append(i)
+print(k)
