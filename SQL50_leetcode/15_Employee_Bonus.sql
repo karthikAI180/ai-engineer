d01f98cda149993b9e6effecfd6f12664300f4da
karthikAI <karthikari180@gmail.com>
2026-06-04 00:39:38 -0400

    Day 12 SQL Practice Done

---
+++ SQL50_leetcode/15_Employee_Bonus.sql
@@ -0,0 +1,7 @@
+select e.name,b.bonus
+from Employee e
+left join Bonus b
+on e.empId=b.empId
+WHERE COALESCE(b.bonus, 0) < 1000
+
+
---
+++ SQL50_leetcode/16_Students_and_Examinations.sql
@@ -0,0 +1,9 @@
+select st.student_id,st.student_name,su.subject_name,
+count(e.student_id) as attended_exams
+from Students st
+cross join Subjects su
+left join Examinations e
+on st.student_id=e.student_id and su.subject_name=e.subject_name
+group by st.student_id,su.subject_name
+order by st.student_id,su.subject_name
+ 
