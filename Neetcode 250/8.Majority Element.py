0949ecd13dcf231f185d526e4db10b11de196954
karthikAI <karthikari180@gmail.com>
2026-06-10 22:29:06 -0400

    Day 18 Neetcode Practice Done

---
+++ Neetcode 250/8.Majority Element.py
@@ -0,0 +1,28 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #Method 1
+        '''
+        k={}
+        res=0
+        maxcount=0
+        for i in nums:
+            k[i]=k.get(i,0)+1
+            res=i if k[i]>maxcount else res
+            maxcount=max(k[i],maxcount)
+        print(k)
+        # for k,v in k.items():
+        #     if v>len(nums)/2:
+        #         return k
+        return res
+        '''
+        #Method 2
+        count=1
+        res=nums[0]
+        for i in range(1,len(nums)):
+            if nums[i]==res:
+                count+=1
+            else:
+                count-=1
+            if count<0:
+                res=nums[i]
+        return res
