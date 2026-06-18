2b5f61fa8fb7f51f250a2fad7c503c6dc8d61f3f
karthikAI <karthikari180@gmail.com>
2026-06-18 10:48:57 -0400

    Day 27 Neetcode Practice Done

---
+++ Neetcode 250/Two pointers/6.Remove Duplicates From Sorted Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        j=1
+        while j<len(nums):
+            if nums[j]!=nums[j-1]:
+                nums[i+1]=nums[j]
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return i+1
+        
