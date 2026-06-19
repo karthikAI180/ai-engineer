4be72aec99e183096d40bc563b7824d32162660f
karthikAI <karthikari180@gmail.com>
2026-06-18 22:45:06 -0400

    Day 18 done

--- Day18/practice.py
+++ Day18/practice.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import chain,combinations,product
 def csv_batch_reader(filename,size):
     with open(filename,'r') as f:
         f=csv.reader(f)
@@ -9,12 +10,15 @@ def csv_batch_reader(filename,size):
             if len(batch)==size:
                 yield batch
                 batch=[]
-
-                
-
 k=csv_batch_reader('C:/Users/kredd/AI_180/AI-portfolio/01-heart-disease-eda/data/heart.csv',10)
 for i in range(10):
     print(next(k))
+print(list(chain([1,2,3],[4,5,6])))
+print(list(combinations(['a','b','c','d'],2)))
+print(list(product([1,2],['x','y'])))
+
+
+
 
 
 
